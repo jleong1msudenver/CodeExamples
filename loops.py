3d3f593f1915d329a9b6002f46d38e29e61f739e
jleong1msudenver <jleong1@msudenver.edu>
2021-02-09 19:21:28 -0700

    reverse string

--- loops.py
+++ loops.py
@@ -1,10 +1,13 @@
 # Take a string as an input and, using a for loop, print the reverse of the string.
-
 # Hint: in what direction should the loop go? 
 
-s = input("Enter a string:")
-step = -1
-stop = -1
-start = len(s) - 1
-for k in range(start,stop,step):
-    print(s[k])
+# s = input("Enter a string:")
+
+# for k in range(len(s) - 1, -1, -1):
+#     print(s[k])
+
+# Problem 3
+alist = [1,2,3,"hello",10.5,"Wow!"]
+
+for s in range(len(alist)-1, -1, -1):
+	print(alist[s])
